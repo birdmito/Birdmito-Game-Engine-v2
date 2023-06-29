8948fa8137bf50300d9099164e4dc83595653abd
Sprite37 <1466726455@qq.com>
2023-06-29 11:03:15 +0800

    update

---
+++ src/behaviours/_DemoBehaviour.ts
@@ -0,0 +1,12 @@
+import { Behaviour } from "../engine/Behaviour";
+import { Transform } from "../engine/Transform";
+
+export class _DemoBehaviour extends Behaviour {
+    onStart(): void {
+
+    }
+
+    onUpdate(): void {
+
+    }
+}
