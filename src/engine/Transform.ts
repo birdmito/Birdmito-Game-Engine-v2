9f4c7d21f9dfd2f8b9a77733520dd943d5fa3cdd
Yusi Ding <bird15600660082@126.com>
2023-07-02 00:37:47 +0800

    修改了Transform和TransformSystem在Transform上添加了九宫格锚点功能，可以支持设置锚点类型，目前仅支持自设包围盒

--- src/engine/Transform.ts
+++ src/engine/Transform.ts
@@ -1,6 +1,7 @@
 import { Matrix } from "../engine";
 import { Behaviour } from "./Behaviour";
 import { number } from "./validators/number";
+import { string } from "./validators/string";
 
 
 
@@ -25,4 +26,14 @@ export class Transform extends Behaviour {
     @number()
     rotation = 0;
 
+    // 锚点
+    // -----------------------------
+    anchor: { x: number, y: number } = { x: 0, y: 0 };
+    // 锚点类型：左上角，中上，右上角，左中，中心，右中，左下角，中下，右下角
+    @string()
+    anchorType: 'left-top' | 'center-top' | 'right-top' | 'left-center' | 'center' | 'right-center' | 'left-bottom' | 'center-bottom' | 'right-bottom' = 'left-top';
+    @string()
+    width = '1920';
+    @string()
+    height = '1080';
 }
