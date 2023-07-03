0ddc114a506ecec993313c9b2ffb72ec143e3046
Merkamel <15669191232@163.com>
2023-07-03 12:20:02 +0800

    scrollbar上下移动实现

--- src/behaviours/ScorllBarBehaviour.ts
+++ src/behaviours/ScorllBarBehaviour.ts
@@ -1,23 +1,45 @@
-import { getGameObjectById } from "../engine";
+import { TextArea } from "@microsoft/fast-components";
+import { GameObject, getGameObjectById } from "../engine";
 import { Behaviour } from "../engine/Behaviour";
+import { TextRenderer } from "../engine/TextRenderer";
+import { Transform } from "../engine/Transform";
 
 export class ScrollBarBehaviour extends Behaviour {
+
+    textArea: GameObject
+    up: GameObject
+    down:GameObject
+    container : GameObject
+    scrollPosition: number
+    scrollOffset: number
+
     onStart(): void {
-        
+
+        this.container = this.gameObject.getChildById('Background')
+        this.textArea = this.gameObject.getChildById('TextArea')
+        this.up = this.gameObject.getChildById('ScrollbarUp')
+        this.down = this.gameObject.getChildById('ScrollbarDown')
     }
 
     onUpdate(): void {
 
-        this.gameObject.onMouseLeftDown = () =>{
-            console.log('container is clicked')
+        // 监听文本区域的鼠标点击事件
+        this.textArea.onMouseLeftDown = () => {
+        console.log('TextArea is clicked');
+        // 处理文本区域被点击的逻辑
         }
 
-        this.gameObject.children[0].onMouseLeftDown = () => {
-            console.log('textArea is clicked')
+        // 监听滚动条的拖动事件
+        this.up.onMouseLeftDown = () => {
+            const yOffset = 10; // 调整此值以控制向上移动的距离
+            this.textArea.getBehaviour(Transform).y -= yOffset
         }
 
-        this.gameObject.children[0].onMouseLeftDown = () => {
-            console.log('scrollbar is clicked')
+        this.down.onMouseLeftDown = () =>{
+            const yOffset = 10; // 调整此值以控制向上移动的距离
+            this.textArea.getBehaviour(Transform).y += yOffset
         }
+
     }
+
 }
