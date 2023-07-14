ee101981aa6301591c651861eaffa4f93a97da49
Yusi Ding <bird15600660082@126.com>
2023-07-14 08:29:34 +0800

    使用imageChe解决按钮报错问题

--- src/behaviours/UI_ButtonState.ts
+++ src/behaviours/UI_ButtonState.ts
@@ -1,22 +1,42 @@
 import { Behaviour } from "../engine/Behaviour";
 import { BitmapRenderer } from "../engine/BitmapRenderer";
 export class UI_ButtonState extends Behaviour{
+    // OPTIMIZE 更好的按钮状态转换，以防止图片被替换为"_Active_Highlight.png"等问题
+    originSource: string;
     onStart(): void {
+        this.originSource = this.gameObject.getBehaviour(BitmapRenderer).source;
+        console.log(this.originSource)
         // 将source的.png替换为_Active.png
         this.gameObject.onMouseLeftDownList.push(() => {
             const source = this.gameObject.getBehaviour(BitmapRenderer).source.replace('_Highlight.png', '_Active.png');
+            if(!this.engine.resourceManager.imageCache[source]){
+                this.gameObject.getBehaviour(BitmapRenderer).source = this.originSource;
+                return;
+            }
             this.gameObject.getBehaviour(BitmapRenderer).source = source;
         });
         this.gameObject.onMouseLeftUpList.push(() => {
             const source = this.gameObject.getBehaviour(BitmapRenderer).source.replace('_Active.png', '_Highlight.png');
+            if(!this.engine.resourceManager.imageCache[source]){
+                this.gameObject.getBehaviour(BitmapRenderer).source = this.originSource;
+                return;
+            }
             this.gameObject.getBehaviour(BitmapRenderer).source = source;
         });
         this.gameObject.onMouseEnterList.push(() => {
             const source = this.gameObject.getBehaviour(BitmapRenderer).source.replace('.png', '_Highlight.png');
+            if(!this.engine.resourceManager.imageCache[source]){
+                this.gameObject.getBehaviour(BitmapRenderer).source = this.originSource;
+                return;
+            }
             this.gameObject.getBehaviour(BitmapRenderer).source = source;
         });
         this.gameObject.onMouseLeaveList.push(() => {
             const source = this.gameObject.getBehaviour(BitmapRenderer).source.replace('_Highlight.png', '.png');
+            if(!this.engine.resourceManager.imageCache[source]){
+                this.gameObject.getBehaviour(BitmapRenderer).source = this.originSource;
+                return;
+            }
             this.gameObject.getBehaviour(BitmapRenderer).source = source;
         });
     }
